c06c053c15c0d3aaf517b2c358c2d953ab2f7fe9
Joakim Repomaa <mail@j.repomaa.com>
2019-07-25 17:57:55 +0200

    start polling in initializer to prevent multiple polling fibers

--- src/kafka/producer.cr
+++ src/kafka/producer.cr
@@ -9,11 +9,11 @@ module Kafka
     # creates a new kafka handle using provided config.
     # Throws exception on error
     def initialize(conf : Config)
-      @polling = false
       @keep_running = true
       @pErrStr = LibC.malloc(ERRLEN).as(UInt8*)
       @handle = LibKafkaC.kafka_new(LibKafkaC::TYPE_PRODUCER, conf, @pErrStr, ERRLEN)
       raise "Kafka: Unable to create new producer" if @handle == 0_u64
+      start_polling(@handle)
     end
 
     # Set the topic to use for *produce()* calls.
@@ -24,7 +24,6 @@ module Kafka
     end
 
     # enqueues *msg*
-    # Will start internal polling fiber, if not already started.
     # returns true on success.  Raises exception otherwise
     def produce(msg : String, partition : Int32 = LibKafkaC::PARTITION_UNASSIGNED, key : String? = nil, flags : Int32 = LibKafkaC::MSG_FLAG_COPY )
 
@@ -35,27 +34,16 @@ module Kafka
               key, (key.nil? ? 0 : key.size), nil)
       raise "Failed to enqueue message" if res == -1
 
-      start_polling unless @polling
-
       return true
     end
 
     # :nodoc:
-    private def start_polling()
-      return if @polling
-
+    private def start_polling(handle)
       spawn do
-
-        @polling = true
-
         while @keep_running
-
-          LibKafkaC.poll(@handle, 500)
+          LibKafkaC.poll(handle, 500)
         end
-
-        @polling = false
       end
-
     end
 
     # Calls *flush(1000)* and will stop polling fiber, if running.
@@ -66,11 +54,6 @@ module Kafka
 
     def flush(timeout_ms : Int32)
       LibKafkaC.flush(@handle, timeout_ms)
-    end
-
-    # returns true if polling fiber is running
-    def polling() : Bool
-      @polling
     end
 
     # :nodoc:
